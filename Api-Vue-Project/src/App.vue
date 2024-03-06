eef0afb00831fff920724fbf6ce8f0ebd2b858ae
OctavioM3 <octaviom3@nycstudents.net>
2024-03-06 09:18:13 -0500

    dfgdgf

--- Api-Vue-Project/src/App.vue
+++ Api-Vue-Project/src/App.vue
@@ -1,12 +1,13 @@
 <template>
   <BarChart />
+  <HomeView />
 </template>
 
 <script>
-import BarChart from '@/components/BarChart.vue'
-
+import BarChart from '@/components/BarChart.vue';
+import HomeView from './views/HomeView.vue';
 export default {
   name: 'App',
-  components: { BarChart }
+  components: { BarChart, HomeView }
 }
 </script>
--- Api-Vue-Project/src/views/HomeView.vue
+++ Api-Vue-Project/src/views/HomeView.vue
@@ -1,8 +1,23 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
 <script setup>
+import { ref, onMounted } from 'vue';
+const pokemon = ref("https://data.cityofnewyork.us/Public-Safety/Motor-Vehicle-Collisions-Crashes/h9gi-nx95/about_data")
+async function getPokemon() {
+  let res = await fetch(pokemon)
+  let data = await res.json();
+  pokemon.value = data.results;
+}
+
+onMounted(() => {
+  getPokemon();
+})
 </script>
 
-<template>
-  <main>
- 
-  </main>
-</template>
+<style lang="scss" scoped>
+
+</style>
