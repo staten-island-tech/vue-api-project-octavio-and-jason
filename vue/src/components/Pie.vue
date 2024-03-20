3507367fcd094d558d1405c55317537a8440f4fe
js1247 <144455716+js1247@users.noreply.github.com>
2024-03-20 09:14:22 -0400

    Update Pie.vue

---
+++ vue/src/components/Pie.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+      <Bar v-if="loaded" :data="chartData" />
+    </div>
+  </template>
+  
+  <script>
+  import { Pie } from 'vue-chartjs'
+  import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+  
+  ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+  
+  export default {
+    name: 'PieChart',
+    components: { Pie },
+    data: () => ({
+      loaded: false,
+      chartData: null
+    }),
+    async mounted () {
+      this.loaded = false
+  
+      try {
+        const { userlist } = await fetch('https://data.cityofnewyork.us/resource/h9gi-nx95.json')
+        this.chartdata = userlist
+  
+        this.loaded = true
+      } catch (e) {
+        console.error(e)
+      }
+    }
+  }
+  </script>
