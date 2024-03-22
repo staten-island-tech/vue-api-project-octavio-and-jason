64cc9bf0f52ad5112161c978ef72c113be560bda
OctavioM3 <octaviom3@nycstudents.net>
2024-03-22 00:42:07 -0400

    bnbmbn

--- vue/src/components/BarComponent.vue
+++ vue/src/components/BarComponent.vue
@@ -5,13 +5,14 @@
     :data="chartData" -->
 
     <div class="cotainer">
-      <Bar v-if="loaded" :data="chartData" />
+      <Bar v-if="loaded" :data="crash" />
     </div>
 </template>
 
 <script>
-import { Bar } from 'vue-chartjs'
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+import { ref, onMounted } from "vue";
+import { Bar } from 'vue-chartjs';
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js';
 
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 
@@ -21,34 +22,40 @@ const props = defineProps({
 })
 
 export default {
-  name: 'BarChart',
-  components: { Bar },
-  props: {
-  data: () => ({
-    loaded: false,
-    chartData: {
-      type: Object,
-      required: true
-    },
-    chartOptions: {
-      type: Object,
-      default: () => {}
-    }
-  })
-},
-  async mounted () {
-    this.loaded = false
-
-        try {
-          const {list} = await fetch("https://data.cityofnewyork.us/resource/h9gi-nx95.json");
-          this.chartData = list
-
-          this.loaded = true
-        }catch (e) {
-          console.error(e)
+  data() {
+    return {
+      chartData: null,
+      chartLabels: [],
+    };
+  },
+  mounted() {
+    this.getCrashData();
+  },
+  methods: {
+    async getCrashData() {
+      try {
+        const response = await fetch('"https://data.cityofnewyork.us/resource/h9gi-nx95.json')
+        const data = await response.json();
+        this.chartData = data;
+        this.chartLabels = this.chartData.map(crash => crash.borough);
+        this.renderChart();
+      } catch (error) {
+        console.error(error);
         }
+      },
+      renderChart() {
+        new Chart({
+        type: 'bar',
+        data: {
+          labels: this.chartLabels,
+          datasets: [{
+            lable: 'Data'
+          }]
+        }
+      });
       }
     }
+};
           /* console.log(data);
           return data
         } catch (error){
