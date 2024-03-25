b521843eac2a914ba24e131ac185915b3a6096a0
js1247 <144455716+js1247@users.noreply.github.com>
2024-03-25 09:16:06 -0400

    916

--- vue/src/components/BarComponent.vue
+++ vue/src/components/BarComponent.vue
@@ -1,9 +1,4 @@
 <template>
-  <!-- <Bar
-    id="my-chart-id"
-    :options="chartOptions"
-    :data="chartData" -->
-
     <div class="cotainer">
       <Bar 
       id="my-chart-id" 
@@ -50,63 +45,4 @@ export default {
     }
   }
 }
-//   data() {
-//     return {
-//       chartData: null,
-//       chartLabels: [],
-//     };
-//   },
-//   mounted() {
-//     this.getCrashData();
-//   },
-//   methods: {
-//     async getCrashData() {
-//       try {
-//         const response = await fetch('"https://data.cityofnewyork.us/resource/h9gi-nx95.json')
-//         const data = await response.json();
-//         this.chartData = data;
-//         this.chartLabels = this.chartData.map(crash => crash.borough);
-//         this.renderChart();
-//       } catch (error) {
-//         console.error(error);
-//         }
-//       },
-//       renderChart() {
-//         new Chart({
-//         type: 'bar',
-//         data: {
-//           labels: this.chartLabels,
-//           datasets: [{
-//             lable: 'Data'
-//           }]
-//         }
-//       });
-//       }
-//     }
-// };
-          /* console.log(data);
-          return data
-        } catch (error){
-          console.log(error);
-        }
-      }
-      getCrashData();
-    } catch(e) {
-      console.error(e)
-    }
-  }
-} */
-//     return {
-//       chartData: {
-//         labels: [ 'January', 'February', 'March' ],
-//         datasets: [ { data: [40, 20, 12] } ]
-//       },
-//       chartOptions: {
-//         responsive: true
-//       }
-//     }
-//   }
-// }
-
-
 </script>
