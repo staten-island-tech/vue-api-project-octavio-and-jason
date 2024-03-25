6db3b90c8d082d28f9619970f81e67196f80636e
js1247 <jasonshi1247@gmail.com>
2024-03-25 18:11:56 -0400

    611

--- vue/src/components/Pie.vue
+++ vue/src/components/Pie.vue
@@ -1,45 +1,58 @@
 <template>
     <div v-if="loaded" class="bar">
-<Bar
-id="my-chart-id"
-:options="chartOptions"
-:data="chartData"
-/>
-</div>
-</template>
-
-<script>
-import { Bar } from 'vue-chartjs' 
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js';
-
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
-
-export default {
+      <Bar
+        id="my-chart-id"
+        :options="chartOptions"
+        :data="chartData"
+      />
+    </div>
+  </template>
+  
+  <script>
+  import { Bar } from 'vue-chartjs';
+  import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js';
+  
+  ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
+  
+  export default {
     name: 'BarChart',
     components: { Bar },
-    data: () => ({
+    data() {
+      return {
         loaded: false,
         chartData: {
-            labels: [ { data } ],
-            datasets: [ {
-                label: 'Crash',
-                data: []
-            }],
-            async onMounted(){
-                this.loaded=false
-                try{
-                    const res = await fetch ("https://data.cityofnewyork.us/resource/h9gi-nx95.json");
-                    let data = await res.json();
-                } catch(error) {
-                    console.error(error);
-                }
-            }
+          labels: [],
+          datasets: [{
+            label: 'Crash',
+            data: []
+          }]
+        },
+        chartOptions: {
+          responsive: true,
+          maintainAspectRatio: false
         }
-    })
-}
-
-</script>
-
-<style lang="scss" scoped>
-
-</style>
+      };
+    },
+    async mounted() {
+      this.loaded = false;
+      try {
+        const res = await fetch("https://data.cityofnewyork.us/resource/h9gi-nx95.json");
+        const data = await res.json();
+        this.prepareChartData(data);
+        this.loaded = true;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    methods: {
+      prepareChartData(data) {
+        this.chartData.labels = data.map(item => item.year);
+        this.chartData.datasets[0].data = data.map(item => item.crashes);
+      }
+    }
+  };
+  </script>
+  
+  <style lang="scss" scoped>
+  </style>
+  
--- vue/src/views/PieChart.vue
+++ vue/src/views/PieChart.vue
@@ -1,25 +1,58 @@
 <template>
-  <div class="about">
-    <h1>Pie Chart</h1>
+  <div v-if="loaded" class="pie">
+    <Pie
+      id="my-pie-chart-id"
+      :options="chartOptions"
+      :data="chartData"
+    />
   </div>
-  <Pie>
-    
-  </Pie>
 </template>
 
-<script setup>
-import Pie from '@/components/Pie.vue';
+<script>
+import { Pie } from 'vue-chartjs';
+import { Chart as ChartJS, Title, Tooltip, Legend, PieController, CategoryScale, LinearScale } from 'chart.js';
+
+ChartJS.register(Title, Tooltip, Legend, PieController, CategoryScale, LinearScale);
+
+export default {
+  name: 'PieChart',
+  components: { Pie },
+  data() {
+    return {
+      loaded: false,
+      chartData: {
+        labels: [],
+        datasets: [{
+          label: 'Crash',
+          data: []
+        }]
+      },
+      chartOptions: {
+        responsive: true,
+        maintainAspectRatio: false
+      }
+    };
+  },
+  async mounted() {
+    this.loaded = false;
+    try {
+      const res = await fetch("https://data.cityofnewyork.us/resource/h9gi-nx95.json");
+      const data = await res.json();
+      this.prepareChartData(data);
+      this.loaded = true;
+    } catch (error) {
+      console.error(error);
+    }
+  },
+  methods: {
+    prepareChartData(data) {
+      this.chartData.labels = data.map(item => item.year);
+      this.chartData.datasets[0].data = data.map(item => item.crashes);
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
 
-</style>
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
 </style>
